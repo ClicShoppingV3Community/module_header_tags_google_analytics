82c9402e9f86664abeef24544d1396e18c16a094
ClicShopping <admin@clicshopping.org>
2021-08-28 14:43:42 +0200

    ADD TYPAGE

--- includes/modules/header_tags/ht_google_analytics.php
+++ includes/modules/header_tags/ht_google_analytics.php
@@ -16,10 +16,10 @@
 
   class ht_google_analytics
   {
-    public $code;
+    public string $code;
     public $group;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
 
